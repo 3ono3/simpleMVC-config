7eae66385658e2636276b4e42d27513fd00093e4
JingY <Gy725268>
2019-08-21 11:41:34 +0800

    误删除回退

---
+++ mvc-config/src/main/java/com/jing/oa/model/User.java
@@ -0,0 +1,47 @@
+package com.jing.oa.model;
+
+/**
+ * @author GuoJingyuan
+ * @date 2019/8/12
+ */
+public class User {
+    private String userId;
+
+    private String userName;
+
+    private String account;
+
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
